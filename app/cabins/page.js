8b259810c20edc08c84e62410613da0eadea8340
KuPiSzzz <kupisz2013@gmail.com>
2025-06-11 23:50:53 +0200

    Adding Logic

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -2,14 +2,15 @@ import { Suspense } from "react";
 import CabinList from "../_components/CabinList";
 import SpinnerMini from "../_components/SpinnerMini";
 import Spinner from "../_components/Spinner";
+import Filter from "../_components/Filter";
+import ReservationReminder from "../_components/ReservationReminder";
 
 export const metadata = {
-  title: "Cabins",
+  title: "Cabins"
 };
 
-export default async function Page() {
-  // CHANGE
-
+export default function Page({ searchParams }) {
+  const filter = searchParams?.capacity ?? "all";
   return (
     <div>
       <h1 className="text-4xl mb-5 text-accent-400 font-medium">
@@ -23,8 +24,12 @@ export default async function Page() {
         away from home. The perfect spot for a peaceful, calm vacation. Welcome
         to paradise.
       </p>
-      <Suspense fallback={<Spinner />}>
-        <CabinList />
+      <div className="flex justify-end mb-8 ">
+        <Filter />
+      </div>
+      <Suspense fallback={<Spinner />} key={filter}>
+        <CabinList filter={filter} />
+        <ReservationReminder />
       </Suspense>
     </div>
   );
