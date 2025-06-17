2e24bfd3cab460ae431cbf43216a220483fa5c9f
KuPiSzzz <kupisz2013@gmail.com>
2025-06-18 00:12:53 +0200

    Adding Auth

--- app/_components/Reservation.js
+++ app/_components/Reservation.js
@@ -1,12 +1,16 @@
+import { getServerSession } from "next-auth";
 import { getBookedDatesByCabinId, getSettings } from "../_lib/data-service";
 import DateSelector from "./DateSelector";
 import ReservationForm from "./ReservationForm";
+import { authOptions } from "../_lib/auth";
+import LoginMessage from "./LoginMessage";
 
 async function Reservation({ cabin }) {
   const [settings, bookedDates] = await Promise.all([
     getSettings(),
     getBookedDatesByCabinId(cabin.id)
   ]);
+  const session = await getServerSession(authOptions);
   return (
     <div className="grid grid-cols-2 border border-primary-800 min-h-[400px]">
       <DateSelector
@@ -14,7 +18,11 @@ async function Reservation({ cabin }) {
         bookedDates={bookedDates}
         cabin={cabin}
       />
-      <ReservationForm cabin={cabin} />
+      {session.user ? (
+        <ReservationForm cabin={cabin} user={session?.user} />
+      ) : (
+        <LoginMessage />
+      )}
     </div>
   );
 }
