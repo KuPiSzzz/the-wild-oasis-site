8f7b4fe8f1260cd40117aad749e6ded12a029103
KuPiSzzz <kupisz2013@gmail.com>
2025-06-27 23:53:22 +0200

    Adding signout option

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -1,6 +1,10 @@
 "use server";
-import { signIn } from "../_lib/auth-client";
+import { signIn, signOut } from "../_lib/auth-client";
 
 export async function signInAction() {
   await signIn("google", { callbackUrl: "/account" });
+}
+
+export async function signOutAction() {
+  await signOut({ redirectTo: "/" });
 }
